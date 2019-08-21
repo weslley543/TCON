51c192bedc3908db14baa3c3e4365a384294eff7
weslley543 <weslleydrum@hotmail.com>
2019-08-21 15:03:50 -0300

    arquivo de configuaração do banco

---
+++ config/bancoDoProjeto.sql
@@ -0,0 +1,12 @@
+CREATE DATABASE TCON;
+
+create table usuario(
+    cod_usuario INT NOT NULL AUTO_INCREMENT,
+    nome_usuario varchar(100) NOT NULL(60),
+    email varchar(100)
+    senha varchar(60) NOT NULL,
+    tipo_usuario boolean NOT NULL,
+    PRIMARY KEY (cod_usuario)
+
+);
+
