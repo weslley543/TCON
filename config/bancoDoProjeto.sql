9f6bddf4f2dda1cc5a9e6b39bd75162e3ee903b3
weslley543 <weslleydrum@hotmail.com>
2019-08-26 16:56:19 -0300

    script para criação da tabela

--- config/bancoDoProjeto.sql
+++ config/bancoDoProjeto.sql
@@ -14,34 +14,30 @@ create table servicotipo(
     nome_tipo varchar(30),
     data_cadastrou DATETIME,
     data_atualizou DATETIME,
-    PRIMARY KEY (cod_servico_tipo)
-)
+    prioridade int
+);
 create table servico(
     cod_servico INT NOT NULL AUTO_INCREMENT,
     cod_servico_tipo int NOT NULL,
     obs_servico varchar(60),
-    cod_usuario int NOT NULL AUTO_INCREMENT,
+    cod_usuario int NOT NULL,
     data_cadatrada DATETIME NOT NULL,
-    data_concluida DATETIME NOT NULL,
+    data_concluida DATETIME NOT NULL, 
     PRIMARY KEY (cod_servico),
     CONSTRAINT FOREIGN KEY (cod_servico_tipo) REFERENCES servicotipo (cod_servico_tipo),
     CONSTRAINT FOREIGN KEY (cod_usuario) REFERENCES usuario (cod_usuario)
 );
 
 create table area_lazer(
-    cod_area INT NOT NULL AUTO_INCREMENT,
-    desc_area VARCHAR NOT NULL (60),
-    PRIMARY KEY (cod_area)
+    cod_area INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    desc_area VARCHAR (60) NOT NULL 
 );
 
 create table agendamento_area_lazer(
     cod_agendamento INT NOT NULL AUTO_INCREMENT,
-    cod_area_lazer INT NOT NULL AUTO_INCREMENT,
+    cod_area_lazer INT NOT NULL,
     data_agendou DATETIME,
     data_entrega DATETIME,
-    PRIMARY KEY (cod_agendamento)
-    
+    PRIMARY KEY (cod_agendamento),
+    CONSTRAINT FOREIGN KEY (cod_area_lazer) REFERENCES area_lazer (cod_area)
 );
-
-
-
