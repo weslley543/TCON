ebff5c9d661788b9e582edad1ad1c04e02f35e0b
Gustavo S <gserpeloni@hotmail.com>
2019-10-12 20:46:01 -0300

    Componentes_v10

---
+++ View/components/index.php
@@ -0,0 +1,18 @@
+<?php session_start();?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Sistema de login</title>
+	<meta charset="utf-8">
+	<meta name = "viewport" content="widt-device">
+	<link rel="stylesheet" type="text/css" href= "css/style.css">
+</head>
+<body>
+	<?php 
+		include("cmp_sidebar.php");
+        include("cmp_content_wrapper.php");
+        include("cmp_logoutModal.php");
+	?>
+</body>
+</html>
