c9058cd46e0c7b7944956fef7e053e9d8d036322
Weslley de Campos <weslley.campos@unesp.br>
2019-10-12 15:41:38 -0300

    enviando tudo

---
+++ Controllers/Servicos.php
@@ -0,0 +1,15 @@
+<?php
+include('../Model/ServicosDAO.php');
+
+class Servicos{
+    public function inserirServico($dados){
+
+    }
+}
+
+
+switch ($_SERVER['REQUEST_METHOD']){
+    case $_POST && $_POST['op'] == 'registrar' :
+        
+        break;
+}
--- index.php
+++ index.php
@@ -1,134 +1,88 @@
-<html lang="pt-br">
+<!DOCTYPE html>
+<html lang="en">
 
 <head>
-    <meta charset="utf-8">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="assets/css/style.css">
-    <title>TCON - Seja Bem Vindo</title>
+
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+  <meta name="description" content="">
+  <meta name="author" content="">
+
+  <title>SB Admin 2 - Login</title>
+
+  <!-- Custom fonts for this template-->
+  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+  <!-- Custom styles for this template-->
+  <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
+
 </head>
 
-<body>
-    <nav class="nav">
-        <nav class="navbar navbar-dark bg-dark navbar-expand-lg fixed-top" id="navBar">
+<body class="bg-gradient-primary">
 
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    <li class="nav-item">
-                        <a class="nav-link" target="_blank" href="">Inicio</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" target="_blank" href=""> Sobre</a>
-                    </li>
-                    <li class="nav-item">
-                        <a class="nav-link" target="_blank" href="">MVV</a>
-                    </li>
-                </ul>
-                <ul class="navbar-nav flex-row ml-md-auto d-none d-md-flex">
-                    <li class="nav-item">
-                        <a class="nav-link" target="_self" href="View/Cadastro.php">Cadastre-se <span class="sr-only">(current)</span></a>
-                    </li>
-                    <li class="nav-item">
-                        <button id ="botaoLogin"class="btn btn-primary">
-                            Login
-                        </button>
-                    </li>
-                </ul>
-            </div>
-        </nav>
-    </nav>
-    <div class="container">
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Unde incidunt rerum laborum voluptatum voluptatibus corrupti reiciendis in odio iste aspernatur explicabo sed doloremque dolorem alias ducimus iusto, provident architecto saepe?
-    </div>
+  <div class="container">
 
-    <div class="modal fade" id="modalLogin" tabindex="-1" role="dialog" aria-labelledby="modalLoginSobre" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="modalLoginSobre">Login</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <form id="formlogin">
-        <div class="modal-body">
-            <div class="form-group">
-                <label>Email</label>
-                <input type="text" name="email" class="form-control">
+    <!-- Outer Row -->
+    <div class="row justify-content-center">
 
-            </div>
-            <div class="form-group">
-                <label>Senha</label>
-                <input type="password" name="senha" class="form-control">
-            
-            </div>
-         </div>
-      </form>
-      <div class="modal-footer">
-        <button type="button" class="btn btn-primary" id="login">Logar</button>
-        <button type="button" class="btn btn-primary" id="recuperarSenha">Recuperar Senha</button>
-        <span id="erro"></span>  
-    </div>
-    </div>
-  </div>
-</div>
+      <div class="col-xl-10 col-lg-12 col-md-9">
 
-<div class="modal fade" id="modalRec" tabindex="-1" role="dialog" aria-labelledby="modalRecSobre" aria-hidden="true">
-  <div class="modal-dialog" role="document">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="modalRecSobre">Login</h5>
-        <button type="button" class="close" data-dismiss="modal" aria-label="Fechar">
-          <span aria-hidden="true">&times;</span>
-        </button>
-      </div>
-      <form id="formRec" action='Controllers/Usuario.php' method="POST">
-        <div class="modal-body">
-            <div class="form-group">
-                
-                <div class="col-md-12">
-                    <label>Email</label>
-                    <input name="email" type="text" name="email" class="form-control">
-                    <input type=hidden name="op" value="recuperacao">
-                    <button class="btn btn-primary" style="margin-top:5px;">Recuperar</button>
+        <div class="card o-hidden border-0 shadow-lg my-5">
+          <div class="card-body p-0">
+            <!-- Nested Row within Card Body -->
+            <div class="row">
+              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+              <div class="col-lg-6">
+                <div class="p-5">
+                  <div class="text-center">
+                    <h1 class="h4 text-gray-900 mb-4">Bem vindo</h1>
+                  </div>
+                  <form class="user" action="Controllers/Usuario.php" method="POST">
+                    <div class="form-group">
+                      <input type="email" name="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Coloque seu email...">
+                    </div>
+                    <div class="form-group">
+                      <input type="password" name="senha" class="form-control form-control-user" id="exampleInputPassword" placeholder="Senha">
+                    </div>
+                    <input type="hidden" value="op">
+                    <div class="form-group">
+                    </div>
+                    <a href="index.html" class="btn btn-primary btn-user btn-block">
+                      Login
+                    </a>
+                    <hr>
+                  </form>
+                  <hr>
+                  <div class="text-center">
+                    <a class="small" href="forgot-password.html">Esqueceu a sua senha ?</a>
+                  </div>
+                  <div class="text-center">
+                    <a class="small" href="register.html">Registre-se!</a>
+                  </div>
                 </div>
-
+              </div>
             </div>
-         </div>
-      </form>
-    </div>
-  </div>
-</div>
-</body>
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
-<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
-<script src ="assets/js/js.js"></script>
-<script>
-    $('#login').on('click', function(e){
-        e.preventDefault;
-        //console.log($('#formlogin').serialize()+'&op=login');
-        $.ajax({
-            type:'POST',
-            url:'Controllers/Usuario.php',
-            data : $('#formlogin').serialize()+'&op=login',
-            success:function(result){
-                
-                    if(result){
-                        window.location.href = 'View/Pagina_parabens.html'
-                    }else{
-                        $('#erro').html('Senha ou usuário incorretos, você pode restaurar sua senha clicando <a href="../View/recuperacaoSenha.html">aqui</a>')
-                    }
-            }
-        })
-        
-    });
-    $('#recuperarSenha').on('click', function(e){
-        $("#modalLogin").modal('hide');
-        $("#modalRec").modal('show');
+          </div>
+        </div>
 
-    });
-</script>
+      </div>
+
+    </div>
+
+  </div>
+
+  <!-- Bootstrap core JavaScript-->
+  <script src="assets/vendor/jquery/jquery.min.js"></script>
+  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+  <!-- Core plugin JavaScript-->
+  <script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+  <!-- Custom scripts for all pages-->
+  <script src="assets/js/sb-admin-2.min.js"></script>
+
+</body>
+
 </html>
