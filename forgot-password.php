3b5a82cdcb7577d3845ff1fc24cd245780fb880b
Weslley Campos <weslley082@gmail.com>
2019-10-22 23:00:27 -0300

    aaaa

--- forgot-password.php
+++ forgot-password.php
@@ -9,14 +9,14 @@
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin 2 - Login</title>
+  <title>SB Admin 2 - Forgot Password</title>
 
   <!-- Custom fonts for this template-->
   <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
+  <link href="css/sb-admin-2.min.css" rel="stylesheet">
 
 </head>
 
@@ -33,33 +33,27 @@
           <div class="card-body p-0">
             <!-- Nested Row within Card Body -->
             <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
+              <div class="col-lg-6 d-none d-lg-block bg-password-image"></div>
               <div class="col-lg-6">
                 <div class="p-5">
                   <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Bem vindo</h1>
+                    <h1 class="h4 text-gray-900 mb-2">Forgot Your Password?</h1>
+                    <p class="mb-4">We get it, stuff happens. Just enter your email address below and we'll send you a link to reset your password!</p>
                   </div>
-                  <form class="user" action="Controllers/Usuario.php" method="POST">
+                  <form class="user">
                     <div class="form-group">
-                      <input type="email" name="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Coloque seu email...">
+                      <input type="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Enter Email Address...">
                     </div>
-                    <div class="form-group">
-                      <input type="password" name="senha" class="form-control form-control-user" id="exampleInputPassword" placeholder="Senha">
-                    </div>
-                    <input type="hidden" value="op">
-                    <div class="form-group">
-                    </div>
-                    <a href="index.php" class="btn btn-primary btn-user btn-block">
-                      Login
+                    <a href="login.html" class="btn btn-primary btn-user btn-block">
+                      Reset Password
                     </a>
-                    <hr>
                   </form>
                   <hr>
                   <div class="text-center">
-                    <a class="small" href="forgot-password.php">Esqueceu a sua senha ?</a>
+                    <a class="small" href="register.html">Create an Account!</a>
                   </div>
                   <div class="text-center">
-                    <a class="small" href="register.php">Registre-se!</a>
+                    <a class="small" href="login.html">Already have an account? Login!</a>
                   </div>
                 </div>
               </div>
@@ -81,7 +75,7 @@
   <script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
 
   <!-- Custom scripts for all pages-->
-  <script src="assets/js/sb-admin-2.min.js"></script>
+  <script src="js/sb-admin-2.min.js"></script>
 
 </body>
 
