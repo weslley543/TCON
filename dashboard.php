098a87063a4f6f5710b39dfeac03f1ec0df7e106
Weslley Campos <weslley082@gmail.com>
2019-10-31 15:42:32 -0300

    aaaa

--- dashboard.php
+++ dashboard.php
@@ -16,84 +16,94 @@ $logado = $_SESSION['email'];
 <head>
 
   <meta charset="utf-8">
-  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge"> 
   <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   <meta name="description" content="">
   <meta name="author" content="">
 
-  <title>SB Admin 2 - Login</title>
+  <title>TCON - Condominios Inteligentes</title>
 
   <!-- Custom fonts for this template-->
-  <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
+  <link href="View/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
   <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
   <!-- Custom styles for this template-->
-  <link href="assets/css/sb-admin-2.min.css" rel="stylesheet">
+   <link href="View/css/sb-admin-2.min.css" rel="stylesheet">;
+
+
+  <!-- Responsividade para Tabela -->
+  <link href="View/vendor/datatables/dataTables.bootstrap4.min.css" rel="stylesheet">
 
 </head>
 
-<body class="bg-gradient-primary">
+<body id="page-top" style="margin-top: -25px;">
+ <!-- Page Wrapper -->
+  <div id="wrapper">
 
-  <div class="container">
+    <?php
+        $pages_adm = ["cmp_sidebar_1","cmp_content_wrapper_2"];
+        $pages_usr = ["usr_cmp_sidebar_1","usr_cmp_content_wrapper_2"];
 
-    <!-- Outer Row -->
-    <div class="row justify-content-center">
-
-      <div class="col-xl-10 col-lg-12 col-md-9">
-
-        <div class="card o-hidden border-0 shadow-lg my-5">
-          <div class="card-body p-0">
-            <!-- Nested Row within Card Body -->
-            <div class="row">
-              <div class="col-lg-6 d-none d-lg-block bg-login-image"></div>
-              <div class="col-lg-6">
-                <div class="p-5">
-                  <div class="text-center">
-                    <h1 class="h4 text-gray-900 mb-4">Bem vindo</h1>
-                  </div>
-                  <form class="user" action="Controllers/Usuario.php" method="POST">
-                    <div class="form-group">
-                      <input type="email" name="email" class="form-control form-control-user" id="exampleInputEmail" aria-describedby="emailHelp" placeholder="Coloque seu email...">
-                    </div>
-                    <div class="form-group">
-                      <input type="password" name="senha" class="form-control form-control-user" id="exampleInputPassword" placeholder="Senha">
-                    </div>
-                    <input type="hidden" value="op">
-                    <div class="form-group">
-                    </div>
-                    <a href="index.html" class="btn btn-primary btn-user btn-block">
-                      Login
-                    </a>
-                    <hr>
-                  </form>
-                  <hr>
-                  <div class="text-center">
-                    <a class="small" href="forgot-password.html">Esqueceu a sua senha ?</a>
-                  </div>
-                  <div class="text-center">
-                    <a class="small" href="register.html">Registre-se!</a>
-                  </div>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-
-      </div>
+ /*
+        foreach($pages_adm as $pages_adm){
+            include "View/components_adm/".$pages_adm.".php";
+        }
+*/
+              
+        foreach($pages_usr as $pages_usr)
+          include "View/components_user/".$pages_usr.".php";
+        
+  
+    ?>
 
     </div>
+    <!-- End of Page Wrapper -->
 
-  </div>
+    <!-- Ajustes Funcionais :  -->
+    <?php
+        $pages_adm = ["cmp_scrool_top_button_7","cmp_logout_modal_8" ];
+        $pages_usr = ["usr_cmp_scrool_top_button_7","usr_cmp_logout_modal_8"];
 
-  <!-- Bootstrap core JavaScript-->
-  <script src="assets/vendor/jquery/jquery.min.js"></script>
-  <script src="assets/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+/*
+       foreach($pages_adm as $pages_adm){
+            include "View/components_adm/".$pages_adm.".php";
+        }
+*/
+        
+        foreach($pages_usr as $pages_usr)
+           include "View/components_user/".$pages_usr.".php";
 
-  <!-- Core plugin JavaScript-->
-  <script src="assets/vendor/jquery-easing/jquery.easing.min.js"></script>
+    ?>
 
-  <!-- Custom scripts for all pages-->
-  <script src="assets/js/sb-admin-2.min.js"></script>
+
+    <!-- Bootstrap core JavaScript-->
+    <script src="View/vendor/jquery/jquery.min.js"></script>
+    <script src="View/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="View/vendor/jquery-easing/jquery.easing.min.js"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="View/js/sb-admin-2.min.js"></script>
+
+    <!-- Page level plugins -->
+    <script src="View/vendor/chart.js/Chart.min.js"></script>
+
+    <!-- Page level custom scripts -->
+    <script src="View/js/demo/chart-area-demo.js"></script>
+    <script src="View/js/demo/chart-pie-demo.js"></script>
+
+
+     <!-- Page level plugins -->
+     <script src="View/vendor/datatables/jquery.dataTables.min.js"></script>
+      <script src="View/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+            
+  <!-- Page level custom scripts -->
+  <script src="js/demo/datatables-demo.js"></script>
+
+  <!-- Scripts - criados BD2 -->
+
+  <script src="View/js/js_user.js"> </script>
 
 </body>
 
