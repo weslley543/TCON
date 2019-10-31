4fd4f461968fd210b88c1a255318c937d0b307db
Weslley Campos <weslley082@gmail.com>
2019-10-31 15:18:55 -0300

    teste

--- config/bancoDoProjeto.sql
+++ config/bancoDoProjeto.sql
@@ -56,9 +56,7 @@ create table historico_exclusao(
     cod_admin int not null,
     cod_usuario int not null,
     data_exclusao DATE not null
-<<<<<<< HEAD
-=======
-)
+);
 
 
 create table historico_os_realizada(
@@ -71,5 +69,4 @@ create table historico_os_realizada(
     CONSTRAINT FOREIGN KEY (cod_servico) REFERENCES servico (cod_servico),
     CONSTRAINT FOREIGN KEY (cod_usuario) REFERENCES usuario (cod_usuario)
 
->>>>>>> b06bf52290ddb5e997437bc8124f54bc31b94ba3
 );
