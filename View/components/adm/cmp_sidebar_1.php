1cd1098bcb5aebe62e628d939b6144b6a4fb5ab9
Gustavo S <gserpeloni@hotmail.com>
2019-11-21 10:41:44 -0300

    att - cmp_sidebar

--- View/components/adm/cmp_sidebar_1.php
+++ View/components/adm/cmp_sidebar_1.php
@@ -55,11 +55,9 @@ l
         <div id="collapseUtilities" class="collapse" aria-labelledby="headingUtilities" data-parent="#accordionSidebar">
           <div class="bg-white py-2 collapse-inner rounded">
             <h6 class="collapse-header">Tipo:</h6>
-            <a class="collapse-item" href="dashboard.php?ref=cmp_table_servicos_2_1">Segurança (Portaria)</a>
-            <a class="collapse-item" href="dashboard.php?ref=cmp_table_servicos_2_1">Elétrica</a>
-            <a class="collapse-item" href="dashboard.php?ref=cmp_table_servicos_2_1">Manutenção </a>
-            <a class="collapse-item" href="dashboard.php?ref=cmp_table_servicos_2_1">Área de Lazer </a>
-            <a class="collapse-item" href="dashboard.php?ref=cmp_table_servicos_2_1">Outros</a>
+            <a class="collapse-item" href="dashboard.php?ref=cmp_table_servicos_2_1">Criar Serviço</a>
+            <a class="collapse-item" href="dashboard.php?ref=cmp_table_servicos_2_1">Serviços Solicitados</a>
+            <a class="collapse-item" href="dashboard.php?ref=cmp_table_servicos_2_1">Usuários Cadastrados</a>
           </div>
         </div>
       </li>
@@ -85,31 +83,13 @@ l
             <a class="collapse-item" href="register.html">Register</a>
             <a class="collapse-item" href="forgot-password.html">Forgot Password</a>
             <div class="collapse-divider"></div>
-            <h6 class="collapse-header">Utilitarios:</h6>
+            <h6 class="collapse-header">Utilitários:</h6>
             <a class="collapse-item" href="404.html">404 Page</a>
             <a class="collapse-item" href="blank.html">Blank Page</a>
           </div>
         </div>
       </li>
 
-
-
-      <!--  Itens não Utilizados -->
-
-      <!-- Nav Item - Charts 
-      <li class="nav-item">
-        <a class="nav-link" href="charts.html">
-          <i class="fas fa-fw fa-chart-area"></i>
-          <span>Charts</span></a>
-      </li> -->
-      
-
-      <!-- Nav Item - Tables 
-      <li class="nav-item">
-        <a class="nav-link" href="tables.html">
-          <i class="fas fa-fw fa-table"></i>
-          <span>Tables</span></a>
-      </li> -->
 
       <!-- Divider -->
       <hr class="sidebar-divider d-none d-md-block">
