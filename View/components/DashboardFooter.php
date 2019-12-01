cc02e0cc35a65dfe4860dbb80863fb5c44138cb4
Gustavo S <gserpeloni@hotmail.com>
2019-12-01 18:12:36 -0300

    Att - Dashboard Admin

--- View/components/DashboardFooter.php
+++ View/components/DashboardFooter.php
@@ -6,10 +6,12 @@
     <!-- Core plugin JavaScript-->
   
 
+
     <!-- Custom scripts for all pages-->
     <script src="assets/js/sb-admin-2.min.js"></script>
+    <script src="assets\js\funcoesServico.js"> </script>
+    <script src="assets\js\funcoes.js"> </script>
 
-    
 
 
    <!-- Page level plugins -->
--- View/components/adm/cmp_conteudo_dashboard_2_1.php
+++ View/components/adm/cmp_conteudo_dashboard_2_1.php
@@ -72,7 +72,7 @@
 
             <!-- Pending Requests Card Example -->
             <div class="col-xl-3 col-md-6 mb-4">
-              <div class="card border-left-warning shadow h-100 py-2">
+              <div class="card border-left-warning shadow h-100 py-2"  id="divAtraso">
                 <div class="card-body">
                   <div class="row no-gutters align-items-center">
                     <div class="col mr-2">
