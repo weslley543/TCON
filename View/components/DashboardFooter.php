c944e299258a87b6388ba32807470b4fc4f1341f
Weslley Campos <weslley082@gmail.com>
2019-12-01 18:00:15 -0300

    aaa

--- View/components/DashboardFooter.php
+++ View/components/DashboardFooter.php
@@ -15,6 +15,8 @@
    <!-- Page level plugins -->
      <script src="assets/vendor/datatables/jquery.dataTables.min.js"></script>
      <script src="assets/vendor/datatables/dataTables.bootstrap4.min.js"></script>
+     <script src="assets/js/funcoes.js"></script>
+     <script src="assets/js/funcoesServico.js"></script>
 </body>
 
 </html>
